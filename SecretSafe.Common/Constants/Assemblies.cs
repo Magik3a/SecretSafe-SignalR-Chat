9f8d188d87cf9bd9038eac03414fd8065c901f7d
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2016-07-23 09:04:27 +0300

    Added Ninjcet for DI/IOC and common project for constants

---
+++ SecretSafe.Common/Constants/Assemblies.cs
@@ -0,0 +1,7 @@
+﻿namespace Common.Constants
+{
+    public class Assemblies
+    {
+        public const string DataServices = "SecretSafe.DataServices";
+    }
+}
---
+++ SecretSafe.Models/UserRooms.cs
@@ -0,0 +1,19 @@
+﻿namespace Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class UserRooms
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public virtual SecretSafeUser User { get; set; }
+
+
+        public string RoomName { get; set; }
+    }
+}
