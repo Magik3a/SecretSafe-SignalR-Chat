10bb01838c2d6b13cc83e46bdedd2aae81a60d67
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2016-07-30 23:28:20 +0300

    added normal security controller with chat functionalities

--- SecretSafe/App_Start/RouteConfig.cs
+++ SecretSafe/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace SecretSafe
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+             name: "Default2",
+             url: "NormalSecurity/{id}",
+             defaults: new { controller = "NormalSecurity", action = "Index", id = UrlParameter.Optional }
+         );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ SecretSafe/Controllers/SecurityLevels/NormalSecurityController.cs
@@ -0,0 +1,40 @@
+﻿using SecretSafe.DataServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SecretSafe.Controllers
+{
+    public class NormalSecurityController : Controller
+    {
+        private readonly IChatRoomsService chatRoomsService;
+        public NormalSecurityController(IChatRoomsService chatRoomsService)
+        {
+            this.chatRoomsService = chatRoomsService;
+        }
+        // GET: NormalSecurity
+        public ActionResult Index(Guid id)
+        {
+            var roomname = chatRoomsService.GetChatRoomById(id).FirstOrDefault().ChatRoomName;
+            if (User.Identity.IsAuthenticated)
+            {
+                return View("~/Views/Home/Chat.cshtml", "_Layout", new UserTest { username = User.Identity.Name, roomname = roomname });
+            }
+            else
+            {
+                Random rnd = new Random();
+                var username = rnd.Next(1000);
+                return View("~/Views/Home/Chat.cshtml", "_Layout", new UserTest { username = username, roomname = roomname });
+
+            }
+        }
+        public class UserTest
+        {
+            public string username { get; set; }
+
+            public string roomname { get; set; }
+        }
+    }
+}
