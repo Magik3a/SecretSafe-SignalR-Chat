e810f9ec9f315d5e797774c05c488e24565e758a
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2016-09-22 22:21:16 +0300

    Minor changes

--- SecretSafe/Properties/AssemblyInfo.cs
+++ SecretSafe/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.3.7.23")]
+[assembly: AssemblyVersion("2.9.7.23")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
