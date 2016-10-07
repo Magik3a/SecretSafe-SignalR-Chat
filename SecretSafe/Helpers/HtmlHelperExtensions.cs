1ed2881d343a2632a8d6b1363cbb90a7cdf7d1b4
Svetlin Krastanov <svetlin.krastanov90@gmail.com>
2016-10-07 11:29:18 +0300

    Added and fixed multilanguage calling to API

---
+++ SecretSafe/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+public static class HtmlHelperExtensions
+{
+
+    private static HttpClient client = new HttpClient();
+
+    public static string MultiLanguage(this HtmlHelper htmlHelper, int phrase)
+    {
+        var language = CultureInfo.CurrentCulture;
+        HttpResponseMessage response = Task.Run(() => client.GetAsync($"http://localhost:44113/api/Phrases/{phrase}/{language.TwoLetterISOLanguageName}")).Result;
+
+        if (response.IsSuccessStatusCode)
+        {
+            var task = Json.Decode(Task.Run(() => response.Content.ReadAsStringAsync()).Result);
+            return task.PhraseText;
+        }
+        else
+        {
+            return response.StatusCode.ToString();
+        }
+
+    }
+}
---
+++ SecretSafe/Models/KnockoutJSModels/MultiLanguageViewModel.js
@@ -0,0 +1,16 @@
+﻿// Namespace
+//var secretSafe = {};
+
+//secretSafe.multilanguageViewModel;
+
+//$(document).ready(function () {
+//    ko.applyBindings(new MultiLanguageViewModel());
+//});
+
+//function MultiLanguageViewModel() {
+//    var self = this;
+
+//    var baseUri = "https://localhost:443/api/Phrases/2/1";
+
+//    $.getJSON(baseUri, self.multilanguageViewModel);
+//}
